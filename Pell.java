7aa10c5488403f378ad9e1d73fb51bb216cf6334
VipinMamidi <76168508+VipinMamidi@users.noreply.github.com>
2021-12-15 15:39:08 -0500

    Update Pell.java

--- Pell.java
+++ Pell.java
@@ -6,9 +6,18 @@ public class Pell {
 
     public long get(int n) {
         if (n < 0) throw new UnsupportedOperationException("Pell.get is not supported for negative n");
-
-        if (n <= 2)
+        if (n<=2){
             return n;
-        return 2 * get(n - 1) + get(n - 2);
+
+        }
+        long i = 1;
+        long j = 2;
+        long r = 0;
+        for (int l = 3; l<=n;l++){
+            r = 2 * j+i;
+            i=j;
+            j=r;
+        }
+        return r;
     }
 }
